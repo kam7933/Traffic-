ebd3e3b67a9622aba9bf9eaaf9e9bdbace151292
kam <kamps3100@gmail.com>
2019-08-10 12:35:45 -0700

    drop menu

--- assets/javascript/traffic.js
+++ assets/javascript/traffic.js
@@ -12,3 +12,26 @@ var firebaseConfig = {
 
   let database = firebase.database();
   console.log(database);
+
+
+
+
+
+
+  function myFunction(){
+    document. getElementById("myDropdown").classList.toggle("show");
+
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('dropbtn')) {
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      let index;
+      for (index = 0; index < dropdowns.length;index++){
+        let openDropdown= dropdowns[index];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
