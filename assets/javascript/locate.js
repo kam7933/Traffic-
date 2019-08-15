c1b52f436e0ff9db119e994188b18b52dc64269e
Mike Brown <theforgesound@gmail.com>
2019-08-14 21:05:31 -0700

    map centering

--- assets/javascript/locate.js
+++ assets/javascript/locate.js
@@ -1,5 +1,5 @@
-var latitude = 37.870456499999996;
-var longitude = -122.25693639999999;
+var latitude = 39.106667;
+var longitude = -94.676392;
 var boundingBox = `${latitude + .1084}%2c${longitude - .1084}%2c${latitude - .1084}%2c${longitude + .1084}`;
 
 var locate = function locate() {
@@ -8,17 +8,40 @@ var locate = function locate() {
         longitude = position.coords.longitude;
         console.log(`Location Set: Lat: ${latitude}, Long: ${longitude}`);
         boundingBox = `${latitude + .1084}%2c${longitude - .1084}%2c${latitude - .1084}%2c${longitude + .1084}`;
+        // setTimeout(
+            function mapInit() {
+            L.mapquest.key = mqKey;
+    
+            var map = L.mapquest.map('map', {
+              center: [latitude, longitude],
+              layers: L.mapquest.tileLayer('map'),
+              zoom: 11
+            });
+            map.addControl(L.mapquest.control());
+          }
+          mapInit();
+        //   , 5000)
     }
 
     function error() {
         alert('Unable to access your location');
+        function mapInit() {
+            L.mapquest.key = mqKey;
+    
+            var map = L.mapquest.map('map', {
+              center: [latitude, longitude],
+              layers: L.mapquest.tileLayer('map'),
+              zoom: 5
+            });
+            map.addControl(L.mapquest.control());
+          }
+          mapInit();
     }
 
 
     if (!navigator.geolocation) {
         alert('Geolocation is not supported by your browser');
     } else {
-        status.textContent = 'Locating…';
         navigator.geolocation.getCurrentPosition(success, error);
     }
 };
